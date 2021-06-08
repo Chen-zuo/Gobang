812b893d0cea655bf51366becc46b6019eb0c319
Chen <chenzuo@chenzuo-Mac.local>
2021-06-08 16:16:58 +0800

    上传git

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Gobang from './components/gobang'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Gobang></Gobang>
+    </div>
+  );
+}
+
+export default App;
+
