7d949dd73202f5afaa507d60eff86d1c08f0a59a
chenzuo <348826240@qq.com>
2021-07-29 14:50:08 +0800

    <F> 修复 class 组件的逻辑拆分一下

--- src/components/class/index.js
+++ src/components/class/index.js
@@ -5,10 +5,10 @@ export class GobangClass extends Component {
     super(props);
     this.state = {
       border: Array(20).fill(null), //渲染棋盘
-      chessArr: null, //棋盘落子情况
+      chessArr: null, //棋盘上的落子
       palyArr: [
         /*  { row: 0, col: 0, chess: 1, } */
-      ], //已落子的位置
+      ], //所有落子
       chess: null, //当前棋子颜色1:黑 2:白,
       row: 0, //当前点击的横坐标
       col: 0, //当前点击的纵坐标
@@ -23,18 +23,22 @@ export class GobangClass extends Component {
     chessArr.map((item, index) => {
       chessArr[index] = Array(20).fill("");
     });
-    this.setState({
-      chessArr,
-    });
+    this.setState({ chessArr });
   }
 
+  // 下棋
   play = (row, col) => {
     let { palyArr, chess, hasWinner } = this.state;
     if (hasWinner) {
-      setTimeout(() =>{
-        let r = window.confirm("Winner:  " + `${hasWinner == 1 ? "黑棋" : "白棋"}` + "\n" + "是否再来一把？")
-        if(r) window.location.reload()
-      },0)
+      setTimeout(() => {
+        let r = window.confirm(
+          "Winner:  " +
+            `${hasWinner == 1 ? "黑棋" : "白棋"}` +
+            "\n" +
+            "是否再来一把？"
+        );
+        if (r) window.location.reload();
+      }, 0);
       return;
     }
     chess = chess === 1 ? 2 : 1;
@@ -45,99 +49,96 @@ export class GobangClass extends Component {
         chess,
         palyArr: [...palyArr, { row, col, chess }],
       },
-      () => {
-        let { palyArr, chess, chessArr } = this.state;
-        palyArr.map((item) => {
-          chessArr[item.row][item.col] = { ...item };
-        });
-        // 分别对 上下，左右，左斜，右斜 方向进行判断是否产生winner
-        let colCount = 0,
-          rowCount = 0,
-          leftObliqueCount = 0,
-          rightObliqueCount = 0;
-        // 上下
-        for (let i = col + 1; i < 20; i++) {
-          if (chessArr[row][i].chess !== chess) break;
-          colCount++; 
-        }
-        for (let i = col - 1; i >= 0; i--) {
-          if (chessArr[row][i].chess !== chess) break;
-          colCount++;
-        }
-        if (colCount >= 4) {
-          setTimeout(() =>{
-            let r = window.confirm("Winner:  " + `${chess == 1 ? "黑棋" : "白棋"}` + "\n" + "是否再来一把？")
-            if(r) window.location.reload();
-          },0)
-          colCount = 0;
-          this.setState({
-            hasWinner: chess
-          })
-          return;
-        }
-        // 左右
-        for (let i = row + 1; i < 20; i++) {
-          if (chessArr[i][col].chess !== chess) break;
-          rowCount++;
-        }
-        for (let i = row - 1; i >= 0; i--) {
-          if (chessArr[i][col].chess !== chess) break;
-          rowCount++; 
-        }
-        if (rowCount >= 4) {
-          setTimeout(() =>{
-            let r = window.confirm("Winner:  " + `${chess == 1 ? "黑棋" : "白棋"}` + "\n" + "是否再来一把？")
-            if(r) window.location.reload();
-          },0)
-          rowCount = 0;
-          this.setState({
-            hasWinner: chess
-          })
-          return;
-        }
-        // 左斜
-        for (let i = row + 1, j = col - 1; i < 20 && j >= 0; i++, j--) {
-          if (chessArr[i][j].chess !== chess) break;
-          leftObliqueCount++;
-        }
-        for (let i = row - 1, j = col + 1; i >=0 && j < 20; i--, j++) {
-          if (chessArr[i][j].chess !== chess) break;
-          leftObliqueCount++;
-        }
-        if (leftObliqueCount >= 4) {
-          setTimeout(() =>{
-            let r = window.confirm("Winner:  " + `${chess == 1 ? "黑棋" : "白棋"}` + "\n" + "是否再来一把？")
-            if(r) window.location.reload();
-          },0)
-          leftObliqueCount = 0;
-          this.setState({
-            hasWinner: chess
-          })
-          return;
-        }
-        // 右斜
-        for (let i = row + 1, j = col + 1; i < 20 && j < 20; i++, j++) {
-          if (chessArr[i][j].chess !== chess) break;
-          rightObliqueCount++;
-        }
-        for (let i = row - 1, j = col - 1; i <= 0 && j <= 0; i--, j--) {
-          if (chessArr[i][j].chess !== chess) break;
-          rightObliqueCount++;
-        }
-        if (rightObliqueCount >= 4) {
-          setTimeout(() =>{
-            let r = window.confirm("Winner:  " + `${chess == 1 ? "黑棋" : "白棋"}` + "\n" + "是否再来一把？")
-            if(r) window.location.reload();
-          },0)
-          rightObliqueCount = 0;
-          this.setState({
-            hasWinner: chess
-          })
-          return;
-        }
-
-      }
+      () => {this.getWinner(row, col)}
     );
+  };
+  // 下棋后判断是否有胜者
+  getWinner = (row, col) => {
+    let { palyArr, chess, chessArr } = this.state;
+    palyArr.map((item) => {
+      chessArr[item.row][item.col] = { ...item };
+    });
+    // 分别对 上下，左右，左斜，右斜 方向进行判断是否产生winner
+    let colCount = 0
+    // 上下
+    for (let i = col + 1; i < 20; i++) {
+      if (chessArr[row][i].chess !== chess) break;
+      colCount++;
+    }
+    for (let i = col - 1; i >= 0; i--) {
+      if (chessArr[row][i].chess !== chess) break;
+      colCount++;
+    }
+    if (colCount >= 4) {
+      setTimeout(() => {
+        let r = window.confirm("Winner:  " +`${chess == 1 ? "黑棋" : "白棋"}` +"\n" + "是否再来一把？");
+        if (r) window.location.reload();
+      }, 0);
+      colCount = 0;
+      this.setState({ hasWinner: chess });
+      return;
+    }
+    // 左右
+    let rowCount = 0
+    for (let i = row + 1; i < 20; i++) {
+      if (chessArr[i][col].chess !== chess) break;
+      rowCount++;
+    }
+    for (let i = row - 1; i >= 0; i--) {
+      if (chessArr[i][col].chess !== chess) break;
+      rowCount++;
+    }
+    if (rowCount >= 4) {
+      setTimeout(() => {
+        let r = window.confirm(
+          "Winner:  " +`${chess == 1 ? "黑棋" : "白棋"}` +"\n" +"是否再来一把？");
+        if (r) window.location.reload();
+      }, 0);
+      rowCount = 0;
+      this.setState({ hasWinner: chess });
+      return;
+    }
+    // 左斜
+    let leftObliqueCount = 0
+    for (let i = row + 1, j = col - 1; i < 20 && j >= 0; i++, j--) {
+      if (chessArr[i][j].chess !== chess) break;
+      leftObliqueCount++;
+    }
+    for (let i = row - 1, j = col + 1; i >= 0 && j < 20; i--, j++) {
+      if (chessArr[i][j].chess !== chess) break;
+      leftObliqueCount++;
+    }
+    if (leftObliqueCount >= 4) {
+      setTimeout(() => {
+        let r = window.confirm(
+          "Winner:  " +`${chess == 1 ? "黑棋" : "白棋"}` +"\n" +"是否再来一把？");
+        if (r) window.location.reload();
+      }, 0);
+      leftObliqueCount = 0;
+      this.setState({ hasWinner: chess });
+      return;
+    }
+    // 右斜
+    let rightObliqueCount = 0
+    for (let i = row + 1, j = col + 1; i < 20 && j < 20; i++, j++) {
+      if (chessArr[i][j].chess !== chess) break;
+      rightObliqueCount++;
+    }
+    for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+      if (chessArr[i][j].chess !== chess) break;
+      rightObliqueCount++;
+    }
+    if (rightObliqueCount >= 4) {
+      setTimeout(() => {
+        let r = window.confirm(
+          "Winner:  " +`${chess == 1 ? "黑棋" : "白棋"}` +"\n" +"是否再来一把？");
+        if (r) window.location.reload();
+      }, 0);
+      rightObliqueCount = 0;
+      this.setState({ hasWinner: chess });
+      return;
+    }
+
   };
 
   render() {
